504ce03de9844af7173ec17247e35ee63b3b6d84
Gil Biren <gilbiren@gmail.com>
2019-07-30 10:46:02 +0200

    Add Grid

--- Main/Main.ino
+++ Main/Main.ino
@@ -33,19 +33,33 @@ int16_t BootupConfigure() {
 
 /* API to display points, lines and rectangles on the screen */
 void Primitives() {
-  uint16_t LineHeight = 40;
+  uint16_t numberOfLines = 8;
+  uint16_t numberOfRows = 8;
+  uint16_t lineWidth = 4;
+  uint16_t displayHeight = FT_DISPLAYHEIGHT * 16;
+  uint16_t displayWidth = FT_DISPLAYWIDTH * 16;
 
   FTImpl.DLStart();//start the display list. Note DLStart and DLEnd are helper apis, Cmd_DLStart() and Display() can also be utilized.
 
 
   /* display list for line strip */
   FTImpl.ColorRGB(0xFF, 0xFF, 0xFF); //set the color of the line strip to white
-  FTImpl.Begin(FT_LINE_STRIP);//begin lines primitive
-  FTImpl.Vertex2f(((FT_DISPLAYWIDTH / 5) - (LineHeight / 2)) * 16, ((FT_DISPLAYHEIGHT - LineHeight) * 2 / 3) * 16); //starting coordinates
-  FTImpl.Vertex2f(((FT_DISPLAYWIDTH / 5) - (LineHeight / 2)) * 16, ((FT_DISPLAYHEIGHT + LineHeight) * 2 / 3) * 16);
-  FTImpl.Vertex2f(((FT_DISPLAYWIDTH / 5) + (LineHeight / 2)) * 16, ((FT_DISPLAYHEIGHT - LineHeight) * 2 / 3) * 16);
-  FTImpl.Vertex2f(((FT_DISPLAYWIDTH / 5) + (LineHeight / 2)) * 16, ((FT_DISPLAYHEIGHT + LineHeight) * 2 / 3) * 16); //ending coordinates
-  FTImpl.End();//end line strip primitive
+
+  for (uint16_t line = 1; line < numberOfLines; ++line) {
+    uint16_t xPos = displayWidth / numberOfLines * line;
+    FTImpl.Begin(FT_LINE_STRIP);//begin lines primitive
+    FTImpl.Vertex2f(xPos, 0); //starting coordinates
+    FTImpl.Vertex2f(xPos, displayHeight);
+    FTImpl.End();//end line strip primitive
+  }
+
+  for (int row = 1; row < numberOfRows; ++row) {
+    uint16_t yPos = displayHeight / numberOfRows * row;
+    FTImpl.Begin(FT_LINE_STRIP);//begin lines primitive
+    FTImpl.Vertex2f(0, yPos); //starting coordinates
+    FTImpl.Vertex2f(displayWidth, yPos);
+    FTImpl.End();//end line strip primitive
+  }
 
   FTImpl.DLEnd();//end the display list
   FTImpl.Finish();//render the display list and wait for the completion of the DL
